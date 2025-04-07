84ad5741a2e2fa19963d1f657d41eb7c55fbef49
SpRofi <sersemsed@gmail.com>
2025-04-07 10:03:09 +0300

    Update Coding.java

--- src/Coding.java
+++ src/Coding.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Coding {
-    ArrayList<Character> englishLowercaseAlphabet = new ArrayList<>(Arrays.asList('a', 'b', 'c', 'd', 'e',
-            'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
-    ArrayList<Character> englishUppercaseAlphabet = new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
+    ArrayList<Character> englishAlphabet = new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
     int j;
     Character character;
 
@@ -14,21 +14,15 @@ public class Coding {
         for (int i = 0; i < buffer.capacity(); i++) {
             character = (char) buffer.get(i);
 
-            if (englishLowercaseAlphabet.contains(character)) {
-                buffer.put(i, (byte) codingChar(englishLowercaseAlphabet, k));
-                continue;
-            }
-            if (englishUppercaseAlphabet.contains(character)) {
-                buffer.put(i, (byte) codingChar(englishUppercaseAlphabet, k));
-            }
+            if (englishAlphabet.contains(character)) buffer.put(i, (byte) codingChar(englishAlphabet, k));
         }
         return buffer;
     }
 
     private char codingChar(ArrayList<Character> list, int k) {
         j = list.indexOf(character);
-        j = (j + k) % 26;
-        if (j < 0) j = j + 26;
+        j = (j + k) % 52;
+        if (j < 0) j = j + 52;
         return list.get(j);
     }
 }
