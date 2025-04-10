884178205e1ca6ca55478bf6701057bf79c9c690
SpRoffi <sersemsed@gmail.com>
2025-04-10 22:14:26 +0300

    moved the constants into a separate class

--- src/CharCrypt.java
+++ src/CharCrypt.java
@@ -1,28 +1,27 @@
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class CharCrypt {
-    ArrayList<Character> englishAlphabet = new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F',
-            'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-            'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
+
+    Constants constants = new Constants();
     int j;
     Character character;
 
     public ByteBuffer enCrypt(ByteBuffer buffer, int k) {
         for (int i = 0; i < buffer.capacity(); i++) {
             character = (char) buffer.get(i);
-
-            if (englishAlphabet.contains(character)) buffer.put(i, (byte) codingChar(englishAlphabet, k));
+            //englishAlphabet
+            if (constants.getEnglishAlphabet().contains(character)) {
+                buffer.put(i, (byte) codingChar(constants.getEnglishAlphabet(), k));
+            }
         }
         return buffer;
     }
 
     private char codingChar(ArrayList<Character> list, int k) {
         j = list.indexOf(character);
-        j = (j + k) % englishAlphabet.size();
-        if (j < 0) j = j + englishAlphabet.size();
+        j = (j + k) % constants.getEnglishAlphabet().size();
+        if (j < 0) j = j + constants.getEnglishAlphabet().size();
         return list.get(j);
     }
 }
